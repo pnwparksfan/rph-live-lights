88394af732b711f3799875fdf42e3ab8149ac2a3
PNWParksFan <gtaparks@gmail.com>
2020-04-03 22:04:33 -0700

    Added version check

--- LiveLights/EntryPoint.cs
+++ LiveLights/EntryPoint.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.Windows.Forms;
+using System.Diagnostics;
 using Rage;
 using Rage.Native;
 using Rage.Attributes;
@@ -18,11 +19,21 @@ namespace LiveLights
     {
         private static void Main()
         {
+            // Older versions of RPH do not support the EmergencyLighting API properly
+            FileVersionInfo rphVer = FileVersionInfo.GetVersionInfo("ragepluginhook.exe");
+            Game.LogTrivial("Detected RPH " + rphVer.FileVersion);
+            if(rphVer.FileMinorPart < 78)
+            {
+                Game.LogTrivial("RPH 78+ is required to use LiveLights");
+                Game.DisplayNotification($"~y~Unable to load LiveLights~w~\nRagePluginHook version ~b~78~w~ or later is required, you are on version ~b~{rphVer.FileMinorPart}");
+                return;
+            }
+
             AssemblyName pluginInfo = Assembly.GetExecutingAssembly().GetName();
             Game.LogTrivial($"Loaded {pluginInfo.Name} {pluginInfo.Version}");
             if(Settings.MenuKey != Keys.None)
             {
-                Game.LogTrivial("Press " + (Settings.MenuModifier == System.Windows.Forms.Keys.None ? "" : (Settings.MenuModifier.ToString() + " + ")) + Settings.MenuKey.ToString() + " to open the menu");
+                Game.LogTrivial("Press " + (Settings.MenuModifier == Keys.None ? "" : (Settings.MenuModifier.ToString() + " + ")) + Settings.MenuKey.ToString() + " to open the menu");
             } else
             {
                 Game.LogTrivial("Use the OpenLiveLightsMenu console command to open the menu");
