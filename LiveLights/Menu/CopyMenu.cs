8a7b84bac2d6deaae28256f93e2c0e07947ca1db
PNWParksFan <gtaparks@gmail.com>
2020-04-09 23:22:47 -0700

    Added additional copy menu UI items

--- LiveLights/Menu/CopyMenu.cs
+++ LiveLights/Menu/CopyMenu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Drawing;
 
 namespace LiveLights.Menu
 {
@@ -11,7 +13,6 @@ namespace LiveLights.Menu
     using RAGENativeUI;
     using RAGENativeUI.Elements;
     using Utils;
-    using System.Drawing;
 
     internal class CopyMenu
     {
@@ -22,8 +23,27 @@ namespace LiveLights.Menu
             DestinationSirenSelectorMenu.Menu.Subtitle.Caption = "~b~Copy Siren Properties > Select Destination Sirens";
             TargetMenu = new SirenSettingsSelectionMenu(ParentELS);
 
+            allCopyCheckboxes.Add(SequencesCheckbox);
+            allCopyCheckboxes.Add(DeltasCheckbox);
+            allCopyCheckboxes.Add(FlashinessCheckbox);
+            allCopyCheckboxes.Add(RotationCheckbox);
+            allCopyCheckboxes.Add(EnvLightingCheckbox);
+            allCopyCheckboxes.Add(CoronaCheckbox);
+            allCopyCheckboxes.Add(ColorCheckbox);
+            allCopyCheckboxes.Add(SettingEnvCheckbox);
+
             Menu.AddItem(AllPropertiesCheckbox);
-            Menu.AddItem(SequencesCheckbox);
+            foreach (UIMenuCheckboxItem checkbox in allCopyCheckboxes)
+            {
+                checkbox.BackColor = Color.FromArgb(100, Color.DarkGray);
+                checkbox.ForeColor = Color.WhiteSmoke;
+                checkbox.Text = "    " + checkbox.Text;
+                Menu.AddItem(checkbox);
+            }
+
+            AllPropertiesCheckbox.CheckboxEvent += OnAllPropertiesChecked;
+
+
             Menu.AddItem(CopyModeItem);
             TargetMenuItem = TargetMenu.CreateAndBindToSubmenuItem(this.Menu, "Target", "Select target siren setting to copy to/from", true);
             Menu.AddItem(SourceSirenSelector);
@@ -34,8 +54,24 @@ namespace LiveLights.Menu
 
             CopyModeItem.OnListChanged += OnCopyModeChanged;
             CopyItem.Activated += OnCopyActivated;
+            CopyItem.BackColor = Color.DarkGreen;
+            CopyItem.ForeColor = Color.White;
+            CopyItem.HighlightedBackColor = Color.PaleGreen;
+            CopyItem.HighlightedForeColor = Color.DarkGreen;
 
             OnCopyModeChanged(CopyModeItem, 0);
+
+            Menu.RefreshIndex();
+
+        }
+
+        private void OnAllPropertiesChecked(UIMenuCheckboxItem sender, bool Checked)
+        {
+            foreach (UIMenuCheckboxItem checkbox in allCopyCheckboxes)
+            {
+                checkbox.Enabled = !Checked;
+                checkbox.SetLeftBadge(Checked ? UIMenuItem.BadgeStyle.Lock : UIMenuItem.BadgeStyle.None);
+            }
         }
 
         private void OnCopyActivated(UIMenu sender, UIMenuItem selectedItem)
@@ -44,14 +80,22 @@ namespace LiveLights.Menu
             if(source.Exists() && destination.Exists() && destination.IsCustomSetting())
             {
                 EmergencyLight sourceSiren = source.Lights[SourceSiren - 1];
+                bool copyAll = AllPropertiesCheckbox.Checked;
+
                 foreach (int sirenId in DestinationSirens)
                 {
                     EmergencyLight destinationSiren = destination.Lights[sirenId - 1];
 
-                    if (SequencesCheckbox.Checked)
+                    if(copyAll)
                     {
-                        destinationSiren.FlashinessSequence = sourceSiren.FlashinessSequence;
-                        destinationSiren.RotationSequence = sourceSiren.RotationSequence;
+
+                    } else
+                    {
+                        if (SequencesCheckbox.Checked)
+                        {
+                            destinationSiren.FlashinessSequence = sourceSiren.FlashinessSequence;
+                            destinationSiren.RotationSequence = sourceSiren.RotationSequence;
+                        }
                     }
                 }
             } else
@@ -90,13 +134,24 @@ namespace LiveLights.Menu
         public EmergencyLighting ParentELS => ParentMenu.ELS;
         public UIMenu Menu { get; }
 
-        public UIMenuCheckboxItem AllPropertiesCheckbox { get; } = new UIMenuCheckboxItem("All Properties", false, "Copy all properties below");
-        public UIMenuCheckboxItem SequencesCheckbox { get; } = new UIMenuCheckboxItem("Sequences", false, "Copy flash and rotation sequences");
+        // Properties selection checkboxes
+        public UIMenuCheckboxItem AllPropertiesCheckbox { get; } = new UIMenuCheckboxItem("All Siren Properties", false, "Copy all properties for the selected sirens. Use individual checkboxes below to select specific properties to copy.");
+        public UIMenuCheckboxItem SequencesCheckbox { get; } = new UIMenuCheckboxItem("Sequences", false, "Copy sequences and multiples properties on both flash and rotation");
+        public UIMenuCheckboxItem DeltasCheckbox { get; } = new UIMenuCheckboxItem("Deltas", false, "Copy Delta properties on flash and rotation");
+        public UIMenuCheckboxItem FlashinessCheckbox { get; } = new UIMenuCheckboxItem("Flashiness", false, "Copy flashiness properties ~y~except~w~ for sequences, multiples, and deltas");
+        public UIMenuCheckboxItem RotationCheckbox { get; } = new UIMenuCheckboxItem("Rotation", false, "Copy rotation properties ~y~except~w~ for sequences, multiples, and deltas");
+        public UIMenuCheckboxItem EnvLightingCheckbox { get; } = new UIMenuCheckboxItem("Siren Env Lighting", false, "Copy siren-specific environmental lighting properties ~y~except~w~ color");
+        public UIMenuCheckboxItem CoronaCheckbox { get; } = new UIMenuCheckboxItem("Corona", false, "Copy all corona settings including intensity and size");
+        public UIMenuCheckboxItem ColorCheckbox { get; } = new UIMenuCheckboxItem("Color", false, "Copy color setting for env lighting and corona");
+        public UIMenuCheckboxItem SettingEnvCheckbox { get; } = new UIMenuCheckboxItem("Overall Env Lighting", false, "Copy overall environmental lighting settings ~y~for the entire SirenSetting~w~ (not siren-specific settings), including falloff and cone angle");
+        private List<UIMenuCheckboxItem> allCopyCheckboxes = new List<UIMenuCheckboxItem>();
+
+
+        // Copy details
         public UIMenuListItem CopyModeItem { get; } = new UIMenuListItem("Copy Mode", "Which siren setting to copy FROM and TO", new string[] { COPY_MODE_SELF, COPY_MODE_TO_TARGET, COPY_MODE_FROM_TARGET });
         public SirenSettingsSelectionMenu TargetMenu { get; }
         public UIMenuItem TargetMenuItem { get; }
         public UIMenuCustomListItem<int> SourceSirenSelector { get; } = new UIMenuCustomListItem<int>("Source Siren ID", "Select siren ID to copy from the source", Enumerable.Range(1, 20));
-        // public UIMenuCustomListItem<int> DestinationSirenSelector { get; } = new UIMenuCustomListItem<int>("Destination Siren ID", "Select siren ID to copy to the destination", Enumerable.Range(1, 20));
         public SirenIdMultiselectMenu DestinationSirenSelectorMenu { get; } = new SirenIdMultiselectMenu("Copy settings to {siren}. Multiselect enabled.");
         public UIMenuItem DestinationSirenMenuItem { get; } = new UIMenuItem("Destination Siren IDs", "Select siren IDs to copy to the destination");
         public UIMenuItem CopyItem { get; } = new UIMenuItem("~h~Copy~h~", "Copy the selected properties");
