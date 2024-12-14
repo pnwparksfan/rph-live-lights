32029cff0ed4797dc623ecaa06538947864f27e4
PNWParksFan <gtaparks@gmail.com>
2024-12-13 22:05:04 -0800

    Fixed RNUI reference

--- LiveLights/Properties/AssemblyInfo.cs
+++ LiveLights/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 // This is required for Octokit to check the version against github
-[assembly: AssemblyInformationalVersion("1.1")]
+[assembly: AssemblyInformationalVersion("1.2")]
 // [assembly: AssemblyVersion("1.0.0.0")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
